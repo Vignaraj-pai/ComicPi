23f70f4fcb0efa1d7f36b311110c38dde4d1842b
Vignaraj <vignarajpai@outlook.com>
2022-11-01 15:30:52 +0530

    Added a footer and comic grid

--- gamepi/home/views.py
+++ gamepi/home/views.py
@@ -10,23 +10,39 @@ def index(request) :
     ts = str(time)
     url_hash = md5((ts+priv_key+pub_key).encode()).hexdigest()
     n = 3
-    url = f"https://gateway.marvel.com:443/v1/public/events?orderBy=-startDate&limit=3&ts={ts}&apikey=872b0839cb76a040e58d12840a5cc260&hash={url_hash}"
-    
-    current_events = requests.get(url).json()
-
+    events_url = f"https://gateway.marvel.com:443/v1/public/events?orderBy=-startDate&limit=3&ts={ts}&apikey=872b0839cb76a040e58d12840a5cc260&hash={url_hash}"
+    comics_url = f"https://gateway.marvel.com:443/v1/public/comics?orderBy=focDate&limit=16&ts={ts}&apikey=872b0839cb76a040e58d12840a5cc260&hash={url_hash}"
+    current_events = requests.get(events_url).json()
+    all_comics = requests.get(comics_url).json()
+    comic_list = all_comics["data"]["results"]
     event_list = current_events["data"]["results"]
 
-    title = []
-    desc = []
-    img = []
+    event_title = []
+    event_desc = []
+    event_img = []
 
     for i in range(len(event_list)) :
         f = event_list[i]
-        title.append(f['title'])
-        desc.append(f['description'])
-        img.append(f['thumbnail']['path'] + '.' + f['thumbnail']['extension'])
+        event_title.append(f['title'])
+        event_desc.append(f['description'])
+        event_img.append(f['thumbnail']['path'] + '.' + f['thumbnail']['extension'])
+
+    comic_title = []
+    comic_issue = []
+    comic_img = []
+
+    for i in range(len(comic_list)) :
+        f = comic_list[i]
+        comic_title.append(f['title'])
+        comic_issue.append(f['issueNumber'])
+        comic_img.append(f['thumbnail']['path'] + '.' + f['thumbnail']['extension'])
+
     
-    mylist = zip(title, desc, img)
-    context = {'mylist':mylist}
+    events = zip(event_title, event_desc, event_img)
+    comics = zip(comic_title, comic_issue, comic_img)
+
+    context = {'eventlist':events,
+                'comiclist':comics 
+            } 
 
     return render(request, "home/index.html", context)
