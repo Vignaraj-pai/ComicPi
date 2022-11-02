2b2079ed10cb5199d40141d0f671ac84a246787e
Vignaraj <vignarajpai@outlook.com>
2022-11-02 13:20:09 +0530

    Fixed the comic grid bug

--- gamepi/home/views.py
+++ gamepi/home/views.py
@@ -5,13 +5,13 @@ import requests
 
 # Create your views here.
 def index(request) :
-    pub_key = "872b0839cb76a040e58d12840a5cc260"
-    priv_key = "abf619c0eb0d1a5db9b5c29e85ae0589d8ffff0a"
+    pub_key = "f3841a03f678f45641e27f0155dfa4e7"
+    priv_key = "cedf68ccbc6076df412b70be34ac7b61fe4b190f"
     ts = str(time)
     url_hash = md5((ts+priv_key+pub_key).encode()).hexdigest()
     n = 3
-    events_url = f"https://gateway.marvel.com:443/v1/public/events?orderBy=-startDate&limit=3&ts={ts}&apikey=872b0839cb76a040e58d12840a5cc260&hash={url_hash}"
-    comics_url = f"https://gateway.marvel.com:443/v1/public/comics?orderBy=focDate&limit=16&ts={ts}&apikey=872b0839cb76a040e58d12840a5cc260&hash={url_hash}"
+    events_url = f"https://gateway.marvel.com:443/v1/public/events?orderBy=-startDate&limit=3&ts={ts}&apikey=f3841a03f678f45641e27f0155dfa4e7&hash={url_hash}"
+    comics_url = f"https://gateway.marvel.com:443/v1/public/comics?orderBy=focDate&limit=36&ts={ts}&apikey=f3841a03f678f45641e27f0155dfa4e7&hash={url_hash}"
     current_events = requests.get(events_url).json()
     all_comics = requests.get(comics_url).json()
     comic_list = all_comics["data"]["results"]
@@ -28,18 +28,18 @@ def index(request) :
         event_img.append(f['thumbnail']['path'] + '.' + f['thumbnail']['extension'])
 
     comic_title = []
-    comic_issue = []
+    comic_url = []
     comic_img = []
 
     for i in range(len(comic_list)) :
         f = comic_list[i]
         comic_title.append(f['title'])
-        comic_issue.append(f['issueNumber'])
+        comic_url.append(f['urls'][0]['url'])
         comic_img.append(f['thumbnail']['path'] + '.' + f['thumbnail']['extension'])
 
     
     events = zip(event_title, event_desc, event_img)
-    comics = zip(comic_title, comic_issue, comic_img)
+    comics = zip(comic_title, comic_url, comic_img)
 
     context = {'eventlist':events,
                 'comiclist':comics 
